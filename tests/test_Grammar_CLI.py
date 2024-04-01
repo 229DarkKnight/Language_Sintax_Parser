79db34d6089af2009137c9a0c9df9151e1636f56
sabrina-guilarte <158226583+sabrina-guilarte@users.noreply.github.com>
2024-04-01 01:06:33 -0600

    Update test_Grammar_CLI.py

--- tests/test_Grammar_CLI.py
+++ tests/test_Grammar_CLI.py
@@ -4,13 +4,13 @@
 
 # --> Pytest for Grammar and CLI Implementation
 
-from .. import CFG
-#from main.CLI import CLI
+from .. import Grammar
+from .. import CLI
 
 import pytest
 
 def test_init_grammar():
-    general_grammar = CFG.Grammar()
+    general_grammar = Grammar()
 
     #check for correct keys in available_grammars dictionary
     assert general_grammar.available_grammars.get('XML') is not None
@@ -24,22 +24,22 @@ def test_init_grammar():
 
 def test_read_string_from_file():
 
-    assert CFG.Grammar.read_string_from_file('XML_CFG.txt') is not None
-    assert CFG.Grammar.read_string_from_file('HTML_CFG.txt') is not None
-    assert CFG.Grammar.read_string_from_file('MATRIX_CFG.txt') is not None
-    assert CFG.Grammar.read_string_from_file('a.txt') is None
+    assert Grammar.read_string_from_file('XML_CFG.txt') is not None
+    assert Grammar.read_string_from_file('HTML_CFG.txt') is not None
+    assert Grammar.read_string_from_file('MATRIX_CFG.txt') is not None
+    assert Grammar.read_string_from_file('a.txt') is None
 
 def test_save_string_as_dict_def():
 
     general_grammar = CFG.Grammar()
-    assert CFG.Grammar.save_string_as_dict_def('XML_CFG.txt', 'XML', general_grammar.available_grammars) == True
-    assert CFG.Grammar.save_string_as_dict_def('HTML_CFG.txt', 'HTML', general_grammar.available_grammars) == True
-    assert CFG.Grammar.save_string_as_dict_def('MATRIX_CFG.txt', 'MATRIX', general_grammar.available_grammars) == True
-    assert CFG.Grammar.save_string_as_dict_def('a.txt', 'b', general_grammar.available_grammars) == False
+    assert Grammar.save_string_as_dict_def('XML_CFG.txt', 'XML', general_grammar.available_grammars) == True
+    assert Grammar.save_string_as_dict_def('HTML_CFG.txt', 'HTML', general_grammar.available_grammars) == True
+    assert Grammar.save_string_as_dict_def('MATRIX_CFG.txt', 'MATRIX', general_grammar.available_grammars) == True
+    assert Grammar.save_string_as_dict_def('a.txt', 'b', general_grammar.available_grammars) == False
 
-#def test_check_str():
- #   general_grammar = CFG.Grammar()
-  #  assert CLI.check_str('XML_Sample1.txt', 'XML', general_grammar) is not None
-   # assert CLI.check_str('HTML_Sample1.txt', 'HTML', general_grammar) is not None
-    #assert CLI.check_str('Matrix_Sample1.txt', 'MATRIX', general_grammar) is not None
-    #assert CLI.check_str('a.txt', 'b', general_grammar) is None
+def test_check_str():
+    general_grammar = Grammar()
+    assert CLI.check_str('XML_Sample1.txt', 'XML', general_grammar) is not None
+    assert CLI.check_str('HTML_Sample1.txt', 'HTML', general_grammar) is not None
+    assert CLI.check_str('Matrix_Sample1.txt', 'MATRIX', general_grammar) is not None
+    assert CLI.check_str('a.txt', 'b', general_grammar) is None
